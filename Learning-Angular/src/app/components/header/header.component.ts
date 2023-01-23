f47b0e5e9070dde31fe746db42b96989f9c46ec3
AlexanderFransson7 <70211809+festusOp7@users.noreply.github.com>
2023-01-23 10:21:57 +0100

    toggling add form with ui service

--- Learning-Angular/src/app/components/header/header.component.ts
+++ Learning-Angular/src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UiService } from 'src/app/servies/ui.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -7,14 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit{
   title: string = 'Learning-Angular';
+  showAddTask: boolean = false;
+  subscription: Subscription;
 
-  constructor(){}
-
-  ngOnInit(): void {
-
+  constructor(private uiService: UiService){
+    this.subscription = this.uiService
+    .onToggle()
+    .subscribe((value) => (this.showAddTask = value))
   }
 
+  ngOnInit(): void {}
+
   toggleAddTask(){
-    console.log("toggogogle")
+    this.uiService.toggleAddTask()
   }
 }
